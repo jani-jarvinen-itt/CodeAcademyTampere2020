7b1fd232fbf0bd800a6dccd40ec1ab15ff7a8491
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-12-15 11:00:40 +0200

    Lisätty kolme C#-koodiesimerkkiä.

---
+++ DotNet/ArvaaLuku/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ArvaaLuku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tervetuloa Arvaa luku -peliin!");
+            Random rnd = new Random();
+            int oikeaVastaus = rnd.Next(1, 21);
+            Console.WriteLine(oikeaVastaus);
+
+            for (int arvauskerta = 0; arvauskerta < 3; arvauskerta++)
+            {
+                Console.WriteLine("Anna arvauksesi välillä 1-20:");
+                int arvaus = int.Parse(Console.ReadLine());
+
+                if (arvaus < oikeaVastaus)
+                {
+                    Console.WriteLine("Oikea luku on suurempi!");
+                }
+                else if (arvaus > oikeaVastaus)
+                {
+                    Console.WriteLine("Oikea luku on pienempi!");
+                }
+                else
+                {
+                    Console.WriteLine("Arvasit oikein, hyvä!");
+                    break;
+                }
+            }
+
+            Console.WriteLine("Peli on päättynyt.");
+        }
+    }
+}
---
+++ DotNet/Laskin/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Laskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna luku 1:");
+            string syöte = Console.ReadLine();
+
+            Console.WriteLine("Anna luku 2:");
+            string syöte2 = Console.ReadLine();
+
+            int luku1 = int.Parse(syöte);
+            int luku2 = int.Parse(syöte2);
+            int summa = luku1 + luku2;
+            Console.WriteLine(summa);
+        }
+    }
+}
