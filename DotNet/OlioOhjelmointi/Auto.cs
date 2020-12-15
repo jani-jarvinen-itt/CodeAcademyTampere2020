86f0a9e12798243092e45404a61e4c93a2a8c1eb
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-12-15 13:47:29 +0200

    Lisätty C#-kielinen esimerkki olio-ohjelmoinnoista.

---
+++ DotNet/OlioOhjelmointi/Auto.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace OlioOhjelmointi
+{
+    public class Auto
+    {
+        public string Merkki { get; set; }
+
+        public string Malli { get; set; }
+
+        public string Huippunopeus { get; set; }
+
+        public string Väri { get; set; }
+
+        public int Nopeus { get; set; }
+
+        public bool MoottoriKäynnissä { get; set; }
+
+        public void Käynnistä()
+        {
+        }
+
+        public void Sammuta()
+        {
+        }
+
+        public void Kiihdytä()
+        {
+        }
+
+        public void Jarruta()
+        {
+        }
+    }
+}
