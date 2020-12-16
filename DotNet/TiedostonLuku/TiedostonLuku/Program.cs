199655778cfa0b3ba9cdd9b40d593c648eeaf036
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-12-16 10:49:41 +0200

    Lisätty C#-koodiesimerkki, joka lukee tiedostosta numeroita ja laskee näiden summan.

---
+++ DotNet/TiedostonLuku/TiedostonLuku/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace TiedostonLuku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tiedostosumman laskenta alkaa.");
+
+            string tiedostoNimi = @"C:\Academy\Tampere\Git\DotNet\Luvut.txt";
+            string[] rivit = File.ReadAllLines(tiedostoNimi);
+
+            int summa = 0;
+            foreach (string rivi in rivit)
+            {
+                int luku = int.Parse(rivi);
+                summa += luku;
+            }
+
+            Console.WriteLine($"Tiedoston lukujen summa on: {summa}.");
+        }
+    }
+}
