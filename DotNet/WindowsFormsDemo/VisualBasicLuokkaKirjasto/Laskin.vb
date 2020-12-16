633b222a7ad5fbac952d9446498234efab897d6f
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-12-16 15:20:17 +0200

    Lisätty Visual Basic -esimerkki laskentaluokasta, jota kutsutaan C#-koodista.

---
+++ DotNet/WindowsFormsDemo/VisualBasicLuokkaKirjasto/Laskin.vb
@@ -0,0 +1,9 @@
+Public Class Laskin
+
+    Public Function Summa(a As Integer, b As Integer) As Integer
+
+        Return a + b
+
+    End Function
+
+End Class
--- DotNet/WindowsFormsDemo/WindowsFormsDemo/Form1.cs
+++ DotNet/WindowsFormsDemo/WindowsFormsDemo/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VisualBasicLuokkaKirjasto;
 
 namespace WindowsFormsDemo
 {
@@ -22,7 +23,11 @@ namespace WindowsFormsDemo
             int luku1 = int.Parse(textBox1.Text);
             int luku2 = int.Parse(textBox2.Text);
 
-            MessageBox.Show((luku1 + luku2).ToString());
+            Laskin laskin = new Laskin();
+            int summa = laskin.Summa(luku1, luku2);
+            MessageBox.Show(summa.ToString());
+
+            // textBox2.BackColor = Color.Navy;
         }
 
         private void Form1_Load(object sender, EventArgs e)
