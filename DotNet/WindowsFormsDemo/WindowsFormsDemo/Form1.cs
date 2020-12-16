b155015a5fa288a214b75d080451625dfe70876c
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-12-16 14:24:42 +0200

    Lisätty yksinkertainen WinForms-käyttöliittymäesimerkki C#:lla.

---
+++ DotNet/WindowsFormsDemo/WindowsFormsDemo/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int luku1 = int.Parse(textBox1.Text);
+            int luku2 = int.Parse(textBox2.Text);
+
+            MessageBox.Show((luku1 + luku2).ToString());
+        }
+    }
+}
