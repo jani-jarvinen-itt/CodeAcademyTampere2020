3036e88d145e50b48fd877b00e5d8625cdf12ac7
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-02-17 15:43:03 +0200

    Lisätty entity-mallit sekä yksinkertainen API-kontrolleriesimerkki, joka palauttaa tietoja tietokannasta.

---
+++ DotNet/AspNetMvcDemo/Controllers/CustomersApiController.cs
@@ -0,0 +1,21 @@
+﻿using AspNetMvcDemo.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetMvcDemo.Controllers
+{
+    [Route("api/asiakkaat")]
+    [ApiController]
+    public class CustomersApiController : ControllerBase
+    {
+        public List<Customer> KaikkiAsiakkat()
+        {
+            NorthwindContext konteksti = new();
+            return konteksti.Customers.ToList();
+        }
+    }
+}
