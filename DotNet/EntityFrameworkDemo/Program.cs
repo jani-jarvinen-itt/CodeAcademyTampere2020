4572ab4b0ea74c514bb90f282b2e5190d258378b
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-02-17 15:29:14 +0200

    Lisätty LINQ-esimerkkejä Northwind-tietokantahakuihin.

--- DotNet/EntityFrameworkDemo/Program.cs
+++ DotNet/EntityFrameworkDemo/Program.cs
@@ -17,6 +17,25 @@ namespace EntityFrameworkDemo
             lkm = konteksti.Orders.Count();
             Console.WriteLine("Tilausten lukumäärä: " + lkm);
 
+            // harjoitus 2
+            lkm = (from e in konteksti.Employees
+                   where e.City == "London"
+                   select e).Count();
+            Console.WriteLine("Harjoitus 2.1, Lontoossa olevat työntekijät: " + lkm);
+
+            var suomalaiset = (from c in konteksti.Customers
+                               where c.Country == "Finland"
+                               select c).ToList();
+            foreach (var suomalainen in suomalaiset) {
+                Console.WriteLine("Suomalainen asiakas: " + suomalainen.CompanyName);
+            }
+
+            lkm = (from o in konteksti.Orders
+                   where o.OrderDate >= new DateTime(1996,1,1) &&
+                         o.OrderDate < new DateTime(1997,1,1)
+                   select o).Count();
+            Console.WriteLine("Harjoitus 2.3, vuoden 1996 tilausten määrä: " + lkm);
+
             konteksti.Dispose();
             Console.WriteLine("Tietokantayhteys suljettu.");
         }
